c4b9affd7fcd9ff492a5bad8ffd01fd41e72cbac
Logan Wright <logan.william.wright@gmail.com>
2014-02-10 15:32:29 -0500

    ImageAssets

--- SimpleCamDemo/ViewController.m
+++ SimpleCamDemo/ViewController.m
@@ -18,12 +18,28 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    
+    SimpleCam * simpleCam = [[SimpleCam alloc]init];
+    simpleCam.delegate = self;
+    [self presentViewController:simpleCam animated:YES completion:nil];
+    
 }
 
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+#pragma mark SIMPLE CAM DELEGATE
+
+- (void) closeSimpleCamWithImage:(UIImage *)image {
+    if (image) {
+        // closed with image
+    }
+    else {
+        // user backed out w/o image
+    }
 }
 
 @end
