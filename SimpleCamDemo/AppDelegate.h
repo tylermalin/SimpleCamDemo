2f2cc1228ff9b6444e440588a5d1007d58846271
Logan Wright <logan.william.wright@gmail.com>
2014-02-10 15:05:13 -0500

    Initial Commit

---
+++ SimpleCamDemo/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  SimpleCamDemo
+//
+//  Created by Logan Wright on 2/10/14.
+//  Copyright (c) 2014 Logan Wright. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ SimpleCamDemo/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  SimpleCamDemo
+//
+//  Created by Logan Wright on 2/10/14.
+//  Copyright (c) 2014 Logan Wright. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
